67d4737760a8c39cfb3dca9aa06c5d257fe7b937
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-08-12 12:22:49 +0530

    plugins added

---
+++ wp-content/plugins/woo-discount-rules-pro/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'flycart/woo_discount_rules_pro',
+        'pretty_version' => 'dev-8.2-deprecated-error',
+        'version' => 'dev-8.2-deprecated-error',
+        'reference' => '680e31a929438dedeaec4703f2198727b1036bb2',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'flycart/woo_discount_rules_pro' => array(
+            'pretty_version' => 'dev-8.2-deprecated-error',
+            'version' => 'dev-8.2-deprecated-error',
+            'reference' => '680e31a929438dedeaec4703f2198727b1036bb2',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yahnis-elsts/plugin-update-checker' => array(
+            'pretty_version' => 'v4.9',
+            'version' => '4.9.0.0',
+            'reference' => '28f29c940cc22754aff85a99645e3c0a51c097e8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
