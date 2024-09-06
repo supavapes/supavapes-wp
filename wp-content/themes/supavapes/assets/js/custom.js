119ef76690a8fc85b1c2ec49afa4abff6cabb347
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-09-06 13:57:23 +0530

    changes in js

--- wp-content/themes/supavapes/assets/js/custom.js
+++ wp-content/themes/supavapes/assets/js/custom.js
@@ -49,7 +49,7 @@ jQuery(document).ready(function() {
                             if (component.types.includes('country')) {
                                 country = component.long_name;
                             }
-                            if (component.types.includes('administrative_area_level_3')) {
+                            if (component.types.includes('administrative_area_level_1')) {
                                 state = component.long_name;
                             }
                         }
