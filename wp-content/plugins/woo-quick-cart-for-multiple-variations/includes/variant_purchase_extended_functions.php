e9a15ad90b5df67f200f8c12ba3ab34c33d013dc
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-09-12 19:43:25 +0530

    changes

--- wp-content/plugins/woo-quick-cart-for-multiple-variations/includes/variant_purchase_extended_functions.php
+++ wp-content/plugins/woo-quick-cart-for-multiple-variations/includes/variant_purchase_extended_functions.php
@@ -314,7 +314,7 @@ function wqcmv_fetch_product_block_html( $variation_id = 0, $changed_variations 
 							<span class="info-icon-container">
 								<img src="/wp-content/uploads/2024/09/info-icon.png" class="info-icon" alt="Info Icon" style="height: 15px; width: 15px; position: relative;">
 								<div class="price-breakup-popup">
-									<p>Regular Price: <?php echo wc_price($reg_price); ?></p>
+									<!-- <p>Regular Price: <?php //echo wc_price($reg_price); ?></p> -->
 									<?php if (isset($sale_price) && !empty($sale_price)) { ?>
 										<p>Product Price: <?php echo wc_price($sale_price); ?></p>
 									<?php }else {?>
