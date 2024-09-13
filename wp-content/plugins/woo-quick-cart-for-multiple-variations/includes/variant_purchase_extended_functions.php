d9011257dd29a6d18d0ec579e4d11861e9f61ff0
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-09-13 19:12:39 +0530

    changes

--- wp-content/plugins/woo-quick-cart-for-multiple-variations/includes/variant_purchase_extended_functions.php
+++ wp-content/plugins/woo-quick-cart-for-multiple-variations/includes/variant_purchase_extended_functions.php
@@ -57,7 +57,7 @@ function supavapes_calculate_ontario_tax( $vaping_liquid ) {
 	$ontario_tax = 0;
 
 	// Check if vaping_liquid value is greater than 10
-	if ($vaping_liquid > 10) {
+	if ($vaping_liquid >= 10) {
 		// Divide the vaping_liquid value into two parts
 		$first_part = 10;
 		$second_part = $vaping_liquid - $first_part;
@@ -87,7 +87,7 @@ function supavapes_calculate_federal_tax( $vaping_liquid ) {
 	$federal_tax = 0;
 
 	// Check if vaping_liquid value is greater than 10
-	if ($vaping_liquid > 10) {
+	if ($vaping_liquid >= 10) {
 		// Divide the vaping_liquid value into two parts
 		$first_part = 10;
 		$second_part = $vaping_liquid - $first_part;
