d5a87126b1d93baf173b1830c68df63da5b08a10
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-09-17 17:03:28 +0530

    changes

--- wp-content/themes/supavapes/functions.php
+++ wp-content/themes/supavapes/functions.php
@@ -4352,49 +4352,52 @@ if ( ! function_exists( 'supavapes_mini_cart_item_quantity_with_breakdown_callba
 
 		?>
 		<!-- Display product quantity and price -->
-		<span class="quantity"><?php echo sprintf( '%s &times; %s', $quantity, $product_price ); ?></span>
+		<span class="quantity">
+			<?php echo sprintf( '%s &times; %s', $quantity, $product_price ); ?>
+			<span class="info-icon-container">
+				<img src="/wp-content/uploads/2024/09/info-icon.svg" class="info-icon" alt="Info Icon" style="height: 15px; width: 15px; position: relative;">
+				<div class="price-breakup-popup" style="display: none;">
+					<h5 class="header"><?php esc_html_e( 'Price Breakdown','supavapes' ); ?></h5>
+					<table class="pricetable">
+						<?php if ( isset( $sale_price ) && !empty( $sale_price ) ) { ?>
+							<tr>
+								<td class='leftprice'><?php esc_html_e( 'Product Price', 'supavapes' ); ?></td>
+								<td class='rightprice'><?php echo wc_price( $sale_price ); ?></td>
+							</tr>
+						<?php } else { ?>
+							<tr>
+								<td class='leftprice'><?php esc_html_e( 'Product Price', 'supavapes' ); ?></td>
+								<td class='rightprice'><?php echo wc_price( $reg_price ); ?></td>
+							</tr>
+						<?php } ?>
+						
+						<?php if ( 'Gujarat' !== $state ) { ?>
+							<tr>
+								<td class='leftprice'><?php esc_html_e( 'Federal Excise Tax', 'supavapes' ); ?></td>
+								<td class='rightprice'><?php echo wc_price( $federal_tax ); ?></td>
+							</tr>
+						<?php } else { ?>
+							<tr>
+								<td class='leftprice'><?php esc_html_e( 'Ontario Excise Tax', 'supavapes' ); ?></td>
+								<td class='rightprice'><?php echo wc_price( $ontario_tax ); ?></td>
+							</tr>
+							<tr>
+								<td class='leftprice'><?php esc_html_e( 'Federal Excise Tax', 'supavapes' ); ?></td>
+								<td class='rightprice'><?php echo wc_price( $federal_tax ); ?></td>
+							</tr>
+						<?php } ?>
+
+						<tr class="wholesaleprice">
+							<td class='leftprice'><?php esc_html_e( 'Wholesale Price', 'supavapes' ); ?></td>
+							<td class='rightprice'><?php echo wc_price( $final_price ); ?></td>
+						</tr>
+					</table>
+				</div>
+			</span>
+		</span>
 
 		<!-- Price Breakdown with info icon -->
-		<span class="info-icon-container">
-			<img src="/wp-content/uploads/2024/09/info-icon.svg" class="info-icon" alt="Info Icon" style="height: 15px; width: 15px; position: relative;">
-			<div class="price-breakup-popup" style="display: none;">
-				<h5 class="header"><?php esc_html_e( 'Price Breakdown','supavapes' ); ?></h5>
-				<table class="pricetable">
-					<?php if ( isset( $sale_price ) && !empty( $sale_price ) ) { ?>
-						<tr>
-							<td class='leftprice'><?php esc_html_e( 'Product Price', 'supavapes' ); ?></td>
-							<td class='rightprice'><?php echo wc_price( $sale_price ); ?></td>
-						</tr>
-					<?php } else { ?>
-						<tr>
-							<td class='leftprice'><?php esc_html_e( 'Product Price', 'supavapes' ); ?></td>
-							<td class='rightprice'><?php echo wc_price( $reg_price ); ?></td>
-						</tr>
-					<?php } ?>
-					
-					<?php if ( 'Gujarat' !== $state ) { ?>
-						<tr>
-							<td class='leftprice'><?php esc_html_e( 'Federal Excise Tax', 'supavapes' ); ?></td>
-							<td class='rightprice'><?php echo wc_price( $federal_tax ); ?></td>
-						</tr>
-					<?php } else { ?>
-						<tr>
-							<td class='leftprice'><?php esc_html_e( 'Ontario Excise Tax', 'supavapes' ); ?></td>
-							<td class='rightprice'><?php echo wc_price( $ontario_tax ); ?></td>
-						</tr>
-						<tr>
-							<td class='leftprice'><?php esc_html_e( 'Federal Excise Tax', 'supavapes' ); ?></td>
-							<td class='rightprice'><?php echo wc_price( $federal_tax ); ?></td>
-						</tr>
-					<?php } ?>
-
-					<tr class="wholesaleprice">
-						<td class='leftprice'><?php esc_html_e( 'Wholesale Price', 'supavapes' ); ?></td>
-						<td class='rightprice'><?php echo wc_price( $final_price ); ?></td>
-					</tr>
-				</table>
-			</div>
-		</span>
+		
 
 		<?php
 
