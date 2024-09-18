7c4bbaa8aa6b6d2e7337695fa51fcb9ea2d77c3b
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-09-18 12:04:49 +0530

    changes

--- wp-content/themes/supavapes/functions.php
+++ wp-content/themes/supavapes/functions.php
@@ -4218,7 +4218,7 @@ if ( ! function_exists( 'supavapes_detail_page_price_breakdown_callback' ) ) {
 		}
 			if( $product_type == 'simple' ){
 
-			remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+			// remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
 			// Get necessary price details
 			$reg_price  = $product->get_regular_price();
 			$sale_price = $product->get_sale_price();
