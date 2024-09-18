d28440f584c9c1189d9c28a38741fa476fa69bac
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-09-18 16:50:59 +0530

    changes

--- wp-content/themes/supavapes/templates/callback_functions/quick-view.php
+++ wp-content/themes/supavapes/templates/callback_functions/quick-view.php
@@ -5,6 +5,29 @@ global $product;
         $product_id = intval( $_POST['product_id'] );
 		$product = wc_get_product( $product_id );
 		$rating = $product->get_average_rating();
+		$sale_price = $product->get_sale_price();
+		$reg_price = $product->get_regular_price();
+
+		$vaping_liquid = get_post_meta( $product_id, '_vaping_liquid', true );
+		$vaping_liquid = (int) $vaping_liquid;
+
+		// Calculate taxes using the custom functions if vaping_liquid is set.
+		if ( isset( $vaping_liquid ) && ! empty( $vaping_liquid ) ) {
+			$ontario_tax = supavapes_calculate_ontario_tax( $vaping_liquid );
+			$federal_tax = supavapes_calculate_federal_tax( $vaping_liquid );
+		}
+
+
+		// Determine the final price based on the state.
+		$state = isset( $_COOKIE['user_state'] ) ? sanitize_text_field( $_COOKIE['user_state'] ) : '';
+
+		if ( 'Gujarat' !== $state ) {
+			$final_price = isset( $sale_price ) && ! empty( $sale_price ) ? floatval( $sale_price ) : floatval( $reg_price );
+			$final_price += floatval( $federal_tax );
+		} else {
+			$final_price = isset( $sale_price ) && ! empty( $sale_price ) ? floatval( $sale_price ) : floatval( $reg_price );
+			$final_price += floatval( $ontario_tax ) + floatval( $federal_tax );
+		}
        ?>
 	   <?php if ( $product && method_exists( $product, 'get_type' ) ) {
 				// $product_type = $product->get_type(); 
@@ -70,6 +93,50 @@ global $product;
 						}
 					?>
 				</div>
+				<?php if ($product->is_type('simple')) { ?>
+                <?php if ( isset( $vaping_liquid ) && !empty( $vaping_liquid ) && $vaping_liquid >= 10 ) {
+							?>
+                <div class="info-icon-container">
+                    <img src="/wp-content/uploads/2024/09/info-icon.svg" class="info-icon" alt="Info Icon" style="height: 15px; width: 15px; position: relative;">
+                    <div class="price-breakup-popup">
+                    <h5 class="header"><?php esc_html_e( 'Price Breakdown','supavapes' ); ?></h5>
+                        <table class="pricetable">
+                        <?php if ( isset( $sale_price ) && !empty( $sale_price ) ) { ?>
+                        <tr>
+                        <td class='leftprice'><?php esc_html_e( 'Product Price','supavapes' ); ?></td>
+                        <td class='rightprice'><?php echo wc_price( $sale_price ); ?></td>
+                        </tr>
+                        <?php }else{?>
+                        <tr>
+                        <td class='leftprice' ><?php esc_html_e( 'Product Price','supavapes' ); ?></td>
+                        <td class='rightprice'><?php echo wc_price( $reg_price ); ?></td>
+                        </tr>
+                        <?php }?>
+                        <?php if ( 'Gujarat' !== $state ) { ?>
+                        <tr>
+                        <td class='leftprice'><?php esc_html_e( 'Federal Excise Tax','supavapes' ); ?></td>
+                        <td class='rightprice'><?php echo wc_price( $federal_tax ); ?></td>
+                        </tr>
+                        <?php }else{?>
+                        <tr>
+                        <td class='leftprice'><?php esc_html_e( 'Ontario Excise Tax','supavapes' ); ?></td>
+                        <td class='rightprice'><?php echo wc_price( $ontario_tax ); ?></td>
+                        </tr>
+                        <tr>
+                        <td class='leftprice'><?php esc_html_e( 'Federal Excise Tax','supavapes' ); ?></td>
+                        <td class='rightprice'><?php echo wc_price( $federal_tax ); ?></td>
+                        </tr>
+                        <?php } ?>
+                        <tr class="wholesaleprice">
+                        <td class='leftprice'><?php esc_html_e( 'Wholesale Price','supavapes' ); ?></td>
+                        <td class='rightprice'><?php echo wc_price( $final_price ); ?></td>
+                        </tr>
+                        </table>
+                    </div>
+                </div>
+            <?php 
+                }
+            }?>
 				<?php
 				if ($product->is_in_stock()) {
 					echo wp_kses_post(wc_get_stock_html($product));
