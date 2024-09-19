fee160168b181b0ab155d1d046a9aa47ff948b60
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-09-19 21:07:56 +0530

    changes

--- wp-content/themes/supavapes/assets/js/custom.js
+++ wp-content/themes/supavapes/assets/js/custom.js
@@ -79,41 +79,21 @@ jQuery(document).ready(function() {
 						success: function(response) {
 						console.log(response);
 							jQuery('.sv-product-slider').html(response);
-							if (jQuery('.sv-product-slider').hasClass('slick-initialized')) {
-								jQuery('.sv-product-slider').slick('unslick');  // Destroy previous instance if any
+							
+							if (jQuery('.sv-product-slider').length) {
+								jQuery('.sv-product-slider').slick({
+									dots: true,
+									arrows: false,
+									infinite: true,
+									autoplay: true,
+									autoplaySpeed: 5000,
+									slidesToShow: 4,
+									slidesToScroll: 4,
+					
+								});
 							}
-							jQuery('.sv-product-slider').slick({
-								dots: true,
-								arrows: false,
-								infinite: true,
-								autoplay: true,
-								autoplaySpeed: 5000,
-								slidesToShow: 4,
-								slidesToScroll: 4,
-								responsive: [
-									{
-										breakpoint: 1024,
-										settings: {
-											slidesToShow: 2,
-											slidesToScroll: 2
-										}
-									},
-									{
-										breakpoint: 992,
-										settings: {
-											slidesToShow: 2,
-											slidesToScroll: 2
-										}
-									},
-									{
-										breakpoint: 575,
-										settings: {
-											slidesToShow: 1,
-											slidesToScroll: 1
-										}
-									}
-								]
-							});
+
+							jQuery('.sv-product-slider').slick('refresh');
 						}
 					});
 
