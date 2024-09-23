c421f4420e923cf27d821c4f1a0ce46b6d527598
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-09-23 16:17:33 +0530

    changes

--- wp-content/themes/supavapes/functions.php
+++ wp-content/themes/supavapes/functions.php
@@ -5255,13 +5255,9 @@ function update_sv_product_slider() {
 
 function supavapes_get_ip_location_and_set_cookies() {
 
-	$REAL_IP = getenv('HTTP_X_REAL_IP');
-	$FORWARDED_CONTINENT = getenv('HTTP_X_FORWARDED_CONTINENT');
-	$FORWARDED_COUNTRY = getenv('HTTP_X_FORWARDED_COUNTRY');
-
-	echo 'Real IP  : '.$REAL_IP.'<br>';
-	echo 'Source IP Continent : '.$FORWARDED_CONTINENT.'<br>';
-	echo 'Source IP Country : '.$FORWARDED_COUNTRY.'<br>';
+	$geoip_record = geoip_record_by_name($_SERVER['REMOTE_ADDR']);
+	$state_code  = $geoip_record['region']; // returns region numerical code
+	debug($state_code);
 	if (!isset($_COOKIE['user_city']) || !isset($_COOKIE['user_state']) || !isset($_COOKIE['user_country'])) {
 
 		$ip_address = $_SERVER['REMOTE_ADDR']; // Get user IP address
