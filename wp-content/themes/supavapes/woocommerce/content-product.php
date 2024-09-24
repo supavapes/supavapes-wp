f06edbe11bcd03c5fded195ce46f85d58c846c1a
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-09-24 19:39:06 +0530

    changes in tax calculation

--- wp-content/themes/supavapes/woocommerce/content-product.php
+++ wp-content/themes/supavapes/woocommerce/content-product.php
@@ -160,9 +160,9 @@ if ( $product->is_type( 'variable' ) ) {
 			<h3 class="sv-our-product-title"><?php echo esc_html($product->get_name(),'supavapes');?></h3>
 			<div class="sv-our-product-price">
             <?php if( $product_type == 'simple' ){ ?>
-			<?php echo wp_kses_post($price); ?>
+			<?php echo wp_kses_post($final_price); ?>
             <?php }else{
-                echo wp_kses_post($final_price);
+                echo wp_kses_post($price);
              }?>
 			<?php if ( $product_data && method_exists( $product_data, 'get_type' ) ) {
 				$product_type = $product_data->get_type();
