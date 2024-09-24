cfe8e158c319c368788a1fcb9f3cc46b2bd6df7b
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-09-24 17:06:56 +0530

    price breakdown html changes

--- wp-content/themes/supavapes/functions.php
+++ wp-content/themes/supavapes/functions.php
@@ -5483,7 +5483,55 @@ if ( ! function_exists( 'supavapes_price_breakdown_html' ) ) {
 	}
 }
 
-supavapes_price_breakdown_html();
+// supavapes_price_breakdown_html();
+
+
+
+function supavapes_price_breakdown_custom_html( $sale_price = null, $reg_price = null, $federal_tax = null, $ontario_tax = null, $final_price = null, $state = '' ) {
+    ob_start(); // Start output buffering
+    ?>
+    <div class="info-icon-container">
+        <img src="/wp-content/uploads/2024/09/info-icon.svg" class="info-icon" alt="Info Icon" style="height: 15px; width: 15px; position: relative;">
+        <div class="price-breakup-popup">
+            <h5 class="header"><?php esc_html_e( 'Price Breakdown', 'supavapes' ); ?></h5>
+            <table class="pricetable">
+                <?php if ( isset( $sale_price ) && !empty( $sale_price ) ) { ?>
+                <tr>
+                    <td class='leftprice'><?php esc_html_e( 'Product Price', 'supavapes' ); ?></td>
+                    <td class='rightprice'><?php echo wc_price( $sale_price ); ?></td>
+                </tr>
+                <?php } else { ?>
+                <tr>
+                    <td class='leftprice'><?php esc_html_e( 'Product Price', 'supavapes' ); ?></td>
+                    <td class='rightprice'><?php echo wc_price( $reg_price ); ?></td>
+                </tr>
+                <?php } ?>
+                <?php if ( 'Gujarat' !== $state ) { ?>
+                <tr>
+                    <td class='leftprice'><?php esc_html_e( 'Federal Excise Tax', 'supavapes' ); ?></td>
+                    <td class='rightprice'><?php echo wc_price( $federal_tax ); ?></td>
+                </tr>
+                <?php } else { ?>
+                <tr>
+                    <td class='leftprice'><?php esc_html_e( 'Ontario Excise Tax', 'supavapes' ); ?></td>
+                    <td class='rightprice'><?php echo wc_price( $ontario_tax ); ?></td>
+                </tr>
+                <tr>
+                    <td class='leftprice'><?php esc_html_e( 'Federal Excise Tax', 'supavapes' ); ?></td>
+                    <td class='rightprice'><?php echo wc_price( $federal_tax ); ?></td>
+                </tr>
+                <?php } ?>
+                <tr class="wholesaleprice">
+                    <td class='leftprice'><?php esc_html_e( 'Wholesale Price', 'supavapes' ); ?></td>
+                    <td class='rightprice'><?php echo wc_price( $final_price ); ?></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <?php
+    return ob_get_clean(); // Return the buffered output
+}
+
 
 
 if ( ! function_exists( 'supavapes_woocommerce_cart_calculate_fees_callback' ) ) {
