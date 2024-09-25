bb72531481f2adc975c22a0f996e521fcd06b043
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-09-25 17:52:31 +0530

    add badge on product detail page

--- wp-content/themes/supavapes/functions.php
+++ wp-content/themes/supavapes/functions.php
@@ -5627,16 +5627,13 @@ function redirect_wp_login_register_to_my_account() {
 }
 
 
+function custom_image_above_featured() {
+    // Define your custom image URL
 
-add_filter( 'woocommerce_sale_flash', 'woocommerce_custom_badge', 10, 3 );
-function woocommerce_custom_badge( $output_html, $post, $product ) {
-
-    // Added compatibility with WC +3
-    $regular_price = method_exists( $product, 'get_regular_price' ) ? $product->get_regular_price() : $product->regular_price;
-    $sale_price = method_exists( $product, 'get_sale_price' ) ? $product->get_sale_price() : $product->sale_price;
-
-    $saved_price = wc_price( $regular_price - $sale_price );
-    $output_html = '<span class="onsale">' . esc_html__( 'Save', 'woocommerce' ) . ' ' . $saved_price . '</span>';
-
-    return $output_html;
+    
+    // Output the custom image
+    echo '<div class="custom-image-above-featured">';
+    echo '<img src="/wp-content/themes/supavapes/assets/images/shop-ontario.png" alt="Custom Image" />';
+    echo '</div>';
 }
+add_action( 'woocommerce_before_single_product_summary', 'custom_image_above_featured', 5 );
