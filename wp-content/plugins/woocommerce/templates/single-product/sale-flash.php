7ecd85c6f85ab24567a501ef4d822a3c00e0f511
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-09-25 17:29:17 +0530

    add badge on product detail page

--- wp-content/plugins/woocommerce/templates/single-product/sale-flash.php
+++ wp-content/plugins/woocommerce/templates/single-product/sale-flash.php
@@ -22,6 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $post, $product;
 
 ?>
+<p>Test text</p>
 <?php if ( $product->is_on_sale() ) : ?>
 
 	<?php echo apply_filters( 'woocommerce_sale_flash', '<span class="onsale">' . esc_html__( 'Sale!', 'woocommerce' ) . '</span>', $post, $product ); ?>
