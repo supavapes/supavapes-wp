e6b24447b3cff8c59c3b4a8c13d7918cb61b4712
PrajapatiPratik-2198 <pratikprajapati2112@gmail.com>
2024-09-25 13:25:36 +0530

    location popup

--- wp-content/themes/supavapes/templates/modals/location-popup.php
+++ wp-content/themes/supavapes/templates/modals/location-popup.php
@@ -3,3 +3,20 @@
  * Location popup. Where user can change/update their location.
  */
 ?>
+<div class="location-popup">
+	<div class="overlay"></div>
+	<div class="location-popup-content">
+		<span class="location-popup-close">
+			<svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M0.715756 20.4308L20.1773 0.969284L22.2927 3.08467L2.83114 22.5462L0.715756 20.4308ZM0.480713 2.80262L2.50208 0.78125L22.4807 20.7599L20.4593 22.7812L0.480713 2.80262Z" fill="white"></path>
+			</svg>				
+		</span>
+		<div class="location-popup-content-box">
+			<div class="location-popup-content-detail">
+				<div class="location-popup-content-main">
+							
+				</div>
+			</div>
+		</div>
+	</div>
+</div> 
