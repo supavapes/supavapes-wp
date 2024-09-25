3a4501f461ad393b1427e6e91ab317fe4bcee507
PrajapatiPratik-2198 <pratikprajapati2112@gmail.com>
2024-09-25 15:49:15 +0530

    location popup

--- wp-content/themes/supavapes/templates/modals/location-popup.php
+++ wp-content/themes/supavapes/templates/modals/location-popup.php
@@ -22,12 +22,13 @@
                         </div>
                         <div class="custom-location-buttons">
                             <a href="" class="button">
-                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            Detect Me
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M9 11.25C10.2426 11.25 11.25 10.2426 11.25 9C11.25 7.75736 10.2426 6.75 9 6.75C7.75736 6.75 6.75 7.75736 6.75 9C6.75 10.2426 7.75736 11.25 9 11.25Z" fill="white"/>
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M2.34333 7.875C2.81754 5.04843 5.04843 2.81754 7.875 2.34333V0H10.125V2.34333C12.9516 2.81754 15.1824 5.04843 15.6566 7.875H18V10.125H15.6566C15.1824 12.9516 12.9516 15.1824 10.125 15.6566V18H7.875V15.6566C5.04843 15.1824 2.81754 12.9516 2.34333 10.125H0V7.875H2.34333ZM4.5 9C4.5 6.51472 6.51472 4.5 9 4.5C11.4852 4.5 13.5 6.51472 13.5 9C13.5 11.4852 11.4852 13.5 9 13.5C6.51472 13.5 4.5 11.4852 4.5 9Z" fill="white"/>
                                 </svg>
                             </a>
-                            <a href="#" class="button"></a>
+                            <a href="#" class="button">Enter Manually</a>
                         </div>
                     </div>
 							
