f532923eaf8e1ba0de0537269f75907b7a2ad24d
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-09-26 11:54:24 +0530

    price breakdown html changes

--- wp-content/themes/supavapes/functions.php
+++ wp-content/themes/supavapes/functions.php
@@ -4307,40 +4307,13 @@ if ( ! function_exists( 'supavapes_detail_page_price_breakdown_callback' ) ) {
 					?>
 					
 				<?php } else { 
-					$range_product_price = wc_price( $min_price ).' - '.wc_price( $max_price );
+					$range_product_price  =  wc_price( $min_price ).' - '.wc_price( $max_price );
+					$range_federal_tax    =  wc_price( $min_federal_tax ).' - '.wc_price( $max_federal_tax );
+					$range_ontario_tax    =  wc_price( $min_ontario_tax ).' - '.wc_price( $max_ontario_tax );
+					$range_final_price    =  wc_price( $final_min_price ).' - '.wc_price( $final_max_price );
+
+					echo supavapes_price_breakdown_custom_html( $range_product_price, $range_federal_tax, $range_ontario_tax, $range_final_price, $state );
 					?>
-					<div class="info-icon-container">
-						<img src="/wp-content/uploads/2024/09/info-icon.svg" class="info-icon" alt="Info Icon" style="height: 15px; width: 15px; position: relative;">
-						<div class="price-breakup-popup">
-							<h5 class="header"><?php esc_html_e( 'Price Breakdown', 'supavapes' ); ?></h5>
-							<table class="pricetable">
-								<tr>
-									<td class='leftprice'><?php esc_html_e( 'Product Price', 'supavapes' ); ?></td>
-									<td class='rightprice'><?php echo $range_product_price; ?></td>
-								</tr>
-								<?php if ( 'Ontario' !== $state ) { ?>
-									<tr>
-										<td class='leftprice'><?php esc_html_e( 'Federal Excise Tax', 'supavapes' ); ?></td>
-										<td class='rightprice'><?php echo wc_price( $min_federal_tax ).' - '.wc_price( $max_federal_tax ); ?></td>
-									</tr>
-								<?php } else { ?>
-									<tr>
-										<td class='leftprice'><?php esc_html_e( 'Ontario Excise Tax', 'supavapes' ); ?></td>
-										<td class='rightprice'><?php echo wc_price( $min_ontario_tax ).' - '.wc_price( $max_ontario_tax ); ?></td>
-									</tr>
-									
-									<tr>
-										<td class='leftprice'><?php esc_html_e( 'Federal Excise Tax', 'supavapes' ); ?></td>
-										<td class='rightprice'><?php echo wc_price( $min_federal_tax ).' - '.wc_price( $max_federal_tax ); ?></td>
-									</tr>
-								<?php } ?>
-								<tr class="wholesaleprice">
-									<td class='leftprice'><?php esc_html_e( 'Total Price', 'supavapes' ); ?></td>
-									<td class='rightprice'><?php echo wc_price( $final_min_price ).' - '.wc_price( $final_max_price ); ?></td>
-								</tr>
-							</table>
-						</div>
-					</div>
 				<?php } 
 				}
 			?>
