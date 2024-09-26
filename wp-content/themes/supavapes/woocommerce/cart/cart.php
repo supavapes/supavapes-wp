26f4b0e17be21e6548ac5d7273fc0c2d3241e119
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-09-26 18:21:07 +0530

    price breakdown discounted price show in info box

--- wp-content/themes/supavapes/woocommerce/cart/cart.php
+++ wp-content/themes/supavapes/woocommerce/cart/cart.php
@@ -72,7 +72,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 					$sale_price = $_product->get_sale_price();
 				}
 				$product_price = $sale_price ? $sale_price : $reg_price; // Use sale price if available, otherwise regular price
-				echo "Price:  ".$product_modified_price = WC()->cart->get_product_price( $_product );
+				$product_modified_price = WC()->cart->get_product_price( $_product );
 
 				// If there is a discount, apply it
 				if ( $applied_discount ) {
@@ -154,7 +154,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 								echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
 							?>
 							<?php if ( isset( $vaping_liquid ) && !empty( $vaping_liquid ) && $vaping_liquid >= 10 ) { 
-								echo supavapes_price_breakdown_custom_html( $product_price, $federal_tax, $ontario_tax, $final_price, $state );
+								echo supavapes_price_breakdown_custom_html( $product_modified_price, $federal_tax, $ontario_tax, $final_price, $state );
 							}?>
 						</td>
 						<td class="product-quantity" data-title="<?php esc_attr_e( 'Quantity', 'supavapes' ); ?>">
