91d8a0e568b32feb8526cd57f9113f52f63ffa36
PrajapatiPratik-2198 <pratikprajapati2112@gmail.com>
2024-09-27 11:41:14 +0530

    location popup

--- wp-content/themes/supavapes/templates/shortcodes/announcement-top-bar.php
+++ wp-content/themes/supavapes/templates/shortcodes/announcement-top-bar.php
@@ -40,7 +40,7 @@ $announcement_bar_button = get_field('announcement_bar_button','option');
 	<div class="location-btn-wrap">
 		<div class="location-btn-wrap-content">
 			<span class="location-country">Gujarat,</span>
-			<span class="location-country">India </span>
+			<span class="location-country">IN</span>
 			<button class="edit-location-btn">
 				<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<circle cx="13" cy="13" r="13" fill="white"/>
