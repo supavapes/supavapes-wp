4aadd2978dd5928fd22ccc7219a562b9f13d007f
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-10-01 15:52:28 +0530

    debugging

--- wp-content/themes/supavapes/functions.php
+++ wp-content/themes/supavapes/functions.php
@@ -4753,7 +4753,7 @@ function supavapes_get_ip_location_and_set_cookies() {
 	// debug($_COOKIE);
 	if ( !isset($_COOKIE['user_city']) && empty($_COOKIE['user_city']) || !isset($_COOKIE['user_state']) && empty($_COOKIE['user_state']) || !isset($_COOKIE['user_country']) && empty($_COOKIE['user_country']) ) {
 		// die('lkoooo');
-		echo "innnnnn";
+		// echo "innnnnn";
 		$ip_address = $_SERVER['REMOTE_ADDR']; // Get user IP address
 		$access_key = '8cccc64e392297'; // Get your free access key from ipinfo.io
 
