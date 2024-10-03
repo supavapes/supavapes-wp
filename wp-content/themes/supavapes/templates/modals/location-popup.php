da481a428ef5f01abb97d5f0d722d324854d18bd
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-10-03 15:33:25 +0530

    changes in location popup

--- wp-content/themes/supavapes/templates/modals/location-popup.php
+++ wp-content/themes/supavapes/templates/modals/location-popup.php
@@ -3,8 +3,14 @@
  * Location popup. Where user can change/update their location.
  */
 
- $location_popup_heading  = get_field( 'location_popup_heading', 'option' );
-?>
+ $location_popup_heading                    =   get_field( 'location_popup_heading', 'option' );
+ $location_address_field_label              =   get_field( 'location_address_field_label', 'option' );
+ $location_update_location_label            =   get_field( 'location_update_location_label', 'option' );
+ $location_detect_me_label                  =   get_field( 'location_detect_me_label', 'option' );
+ $location_enter_manually_label             =   get_field( 'location_enter_manually_label', 'option' );
+ $enter_manually_form_heading               =   get_field( 'enter_manually_form_heading', 'option' );
+ $enter_manually_form_submit_button_label   =   get_field( 'enter_manually_form_submit_button_label', 'option' );
+?> 
 <div class="location-popup">
    <div class="overlay"></div>
    <div class="location-popup-content">
@@ -24,7 +30,7 @@
                <div class="location-popup-header">
                   <h2><?php echo esc_html( $location_popup_heading ); ?></h2>
                   <button type="button" id="update-user-location" class="button submit-location" data-userselectedstate="" data-userselectedcountry="">
-                     <?php esc_html_e( 'Update my location', 'supavapes' ); ?> 
+                     <?php echo esc_html( $location_update_location_label  ); ?> 
                      <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M16.3727 3.56366L13.1727 0.36366C13.1228 0.31423 13.0637 0.275123 12.9988 0.248582C12.9338 0.222041 12.8642 0.208587 12.794 0.208993H2.12734C1.703 0.208993 1.29603 0.377564 0.995973 0.677622C0.695915 0.97768 0.527344 1.38465 0.527344 1.80899V14.609C0.527344 15.0333 0.695915 15.4403 0.995973 15.7404C1.29603 16.0404 1.703 16.209 2.12734 16.209H14.9273C15.3517 16.209 15.7586 16.0404 16.0587 15.7404C16.3588 15.4403 16.5273 15.0333 16.5273 14.609V3.94232C16.5277 3.87213 16.5143 3.80255 16.4877 3.73757C16.4612 3.67259 16.4221 3.61349 16.3727 3.56366ZM10.6607 1.27566V4.47566H6.39401V1.27566H10.6607ZM4.26067 15.1423V11.409C4.26067 11.2675 4.31687 11.1319 4.41688 11.0319C4.5169 10.9318 4.65256 10.8757 4.79401 10.8757H12.2607C12.4021 10.8757 12.5378 10.9318 12.6378 11.0319C12.7378 11.1319 12.794 11.2675 12.794 11.409V15.1423H4.26067ZM15.4607 14.609C15.4607 14.7504 15.4045 14.8861 15.3045 14.9861C15.2044 15.0861 15.0688 15.1423 14.9273 15.1423H13.8607V11.409C13.8607 10.9846 13.6921 10.5777 13.392 10.2776C13.092 9.97756 12.685 9.80899 12.2607 9.80899H4.79401C4.36966 9.80899 3.9627 9.97756 3.66264 10.2776C3.36258 10.5777 3.19401 10.9846 3.19401 11.409V15.1423H2.12734C1.98589 15.1423 1.85024 15.0861 1.75022 14.9861C1.6502 14.8861 1.59401 14.7504 1.59401 14.609V1.80899C1.59401 1.66754 1.6502 1.53189 1.75022 1.43187C1.85024 1.33185 1.98589 1.27566 2.12734 1.27566H5.32734V4.47566C5.32734 4.75855 5.43972 5.02987 5.63976 5.2299C5.8398 5.42994 6.11111 5.54232 6.39401 5.54232H10.6607C10.9436 5.54232 11.2149 5.42994 11.4149 5.2299C11.615 5.02987 11.7273 4.75855 11.7273 4.47566V1.27566H12.5753L15.4607 4.16099V14.609Z" fill="white"/>
                      </svg>
@@ -32,13 +38,13 @@
                </div>
                <div class="custom-location-wrap">
                     <div class="custom-location-input-box">
-                     <label><?php esc_html_e( 'Address', 'supavapes' ); ?></label>
+                     <label><?php echo esc_html(  $location_address_field_label  ); ?></label>
                      <input type="text" placeholder="Enter Your Address" class="type-address" id="pac-input" required="" value="">
                      <span class="address-error-msg"></span>
                   </div>
                   <div class="custom-location-buttons">
-                     <a href="javascript:void(0);" id="detect-me-button"><?php esc_html_e( 'Detect Me', 'supavapes' ); ?></a>
-                     <button class="enter-menual-btn"><?php esc_html_e( 'Enter Manually', 'supavapes' ); ?></button>
+                     <a href="javascript:void(0);" id="detect-me-button"><?php echo esc_html( $location_detect_me_label ); ?></a>
+                     <button class="enter-menual-btn"><?php echo esc_html( $location_enter_manually_label ); ?></button>
                   </div>
                </div>
                <div id="location-map" style="width: 100%; height: 500px;"></div>
@@ -57,7 +63,7 @@
                   </span>
                   <div class="location-popup-form-content-box">
                      <div class="location-popup-form-content-detail">
-                        <h2><?php esc_html_e( 'Please fill the form', 'supavapes' ); ?></h2>
+                        <h2><?php echo esc_html( $enter_manually_form_heading ); ?></h2>
                         <form class="menual-location">
                            <div class="menual-location-form-group">
                               <label><?php esc_html_e( 'Country/Region', 'supavapes' ); ?></label>
@@ -79,7 +85,7 @@
                                     <option value="SK" data-lat="52.9399" data-lng="-106.4509"><?php esc_html_e( 'Saskatchewan', 'supavapes' ); ?></option>
                                 </select>
                             </div>
-                           <button type="button" id="submit-location-form" class="button submit-location-form"><?php esc_html_e( 'Update my location', 'supavapes' ); ?></button>
+                           <button type="button" id="submit-location-form" class="button submit-location-form"><?php echo esc_html( $enter_manually_form_submit_button_label ); ?></button>
                         </form>
                      </div>
                   </div>
