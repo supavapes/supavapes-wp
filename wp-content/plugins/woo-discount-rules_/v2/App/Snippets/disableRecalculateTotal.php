ec6dc02edc6003538dcfdf3d36695037c97ea88b
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-10-04 12:37:22 +0530

    plugin updates

--- wp-content/plugins/woo-discount-rules_/v2/App/Snippets/disableRecalculateTotal.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * @since 2.3.6
- */
-
-add_filter('advanced_woo_discount_rules_do_recalculate_total', '__return_false');
--- wp-content/plugins/woo-discount-rules_/v2/App/Snippets/recalculateTotalBeforeApplyCoupon.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * @since 2.3.6
- */
-
-add_filter('advanced_woo_discount_rules_recalculate_discount_before_apply_coupon', '__return_false');
